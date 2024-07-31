a817ecbc769250625ec7c0466cf3a0129bb73633
Raisa Tathoi <tathoi.raisa@gmail.com>
2024-07-30 19:35:00 -0600

    The thing that taught me django

---
+++ products/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+def index(request):
+    products = Product.objects.all()
+    return render(request,
+                  "index.html",
+                  {"products": products})
+
+
+def new_products(request):
+    return HttpResponse("newbies")
+
+
+
+
+
+
